be49e25510018ac8bd17c197d92e69da5907876f
Moritz Schalk <schalk.moritz@tutanota.de>
2024-03-01 15:30:47 +0100

    Improve timer.

--- timer.js
+++ timer.js
@@ -1,6 +1,7 @@
 export default class Timer {
 
     constructor() {
+        this.iterations = 0;
         this.averageTime = 0;
         this.maxTime = 0;
         this.minTime = Infinity;
@@ -14,10 +15,12 @@ export default class Timer {
     iteration() {
         const now = window.performance.now();
         const delta = now - this.startTime;
+
+        this.iterations++;
         this.maxTime = Math.max(this.maxTime, delta);
         this.minTime = Math.min(this.minTime, delta);
-        this.averageTime = (this.averageTime + delta) / 2;
-        this.startTime = now;
+        this.averageTime = ((this.iterations - 1) * this.averageTime + delta) / this.iterations;
+        this.startTime = window.performance.now();
     }
 
     get results() {
