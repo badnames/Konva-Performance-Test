837aa649d4c240c059e05d3d5bbed18fd2ad2c90
Moritz Schalk <schalk.moritz@tutanota.de>
2024-03-02 10:30:46 +0100

    New default settings.

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const layerCount = 40;
-export const objectsPerLayer = 10;
+export const layerCount = 200;
+export const objectsPerLayer = 50;
 export const repitions = 1000;
 export const useLayers = false;
